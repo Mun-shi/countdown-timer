555240066cb981a3812339cb044e1309b90d044b
Mun-shi <90496225+Mun-shi@users.noreply.github.com>
2022-08-15 15:49:01 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const newYears = "1 jan 2023";
+
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const seconds = (newYearsDate - currentDate) / 1000;
+    const days = Math.floor(seconds / 3600 / 24);
+    const hours = Math.floor(seconds / 3600) % 24;
+
+    console.log(days, hours);
+}
+//initial call
+countdown();
+
+setInterval(countdown, 1000)  
